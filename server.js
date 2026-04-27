db2a64b5f9cc2333fa67ede649898e32afb1d284
Kemerald25 <unityekeoba18@gmail.com>
2026-04-27 06:58:02 +0100

    feat: configure server port using environment variable with fallback to 3000

--- server.js
+++ server.js
@@ -37,8 +37,9 @@ app.prepare().then(() => {
     });
   });
 
-  server.listen(3000, (err) => {
+  const PORT = process.env.PORT || 3000;
+  server.listen(PORT, (err) => {
     if (err) throw err;
-    console.log('> Ready on http://localhost:3000 with Socket.io');
+    console.log(`> Ready on http://localhost:${PORT} with Socket.io`);
   });
 });
